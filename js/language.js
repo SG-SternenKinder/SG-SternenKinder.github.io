ec72b51e123bade8ebd6f0564b038210e7405571
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-10-01 12:06:59 +0200

    ...

--- js/language.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#change-language').on('click', function () {
-      $("body").toggleClass("page-language-mode");
-      localStorage.setItem('osy-language-mode', $("body").hasClass("page-language-mode"));
-    });
-    if (localStorage.getItem('osy-language-mode') === 'true') {
-      $('#change-language').trigger('click');
-    }
-});
