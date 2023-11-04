a8ae59f41cf0830a5308b5abdb0a9649e1c6f477
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-04 21:19:20 +0100

    testo switcher

--- js/js.js
+++ js/js.js
@@ -25,3 +25,33 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     xhr.send();
 });
+
+// Definiere eine Variable für die aktuelle Sprache (zunächst Deutsch)
+let currentLanguage = 'de';
+
+// Funktion zum Ändern der Sprache
+function changeLanguage(lang) {
+    // Setze die aktuelle Sprache
+    currentLanguage = lang;
+
+    // Rufe die Funktion auf, um den Text in der ausgewählten Sprache zu aktualisieren
+    updateLanguageText();
+}
+
+// Funktion zum Aktualisieren des Texts in der ausgewählten Sprache
+function updateLanguageText() {
+    if (currentLanguage === 'de') {
+        // Texte für die Deutsche Sprache
+        document.getElementById('main-title').textContent = 'TEST <br> TEXT';
+        document.getElementById('main-text').textContent = 'DIES IST EIN TEST TEXT';
+        // Füge hier weitere Texte in Deutsch hinzu
+    } else if (currentLanguage === 'en') {
+        // Texte für die Englische Sprache
+        document.getElementById('main-title').textContent = 'TEST <br> TEXT';
+        document.getElementById('main-text').textContent = 'THIS IS A TEST TEXT';
+        // Füge hier weitere Texte in Englisch hinzu
+    }
+}
+
+// Führe die Funktion aus, um den Text in der aktuellen Sprache anzuzeigen
+updateLanguageText();
