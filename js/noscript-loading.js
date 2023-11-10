b9b9a94400fea417f4ec98162c657b1ac3a922df
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-11 00:12:07 +0100

    Update noscript-loading.js

--- js/noscript-loading.js
+++ js/noscript-loading.js
@@ -1,15 +1,21 @@
 // noscript-loading.js
 document.addEventListener('DOMContentLoaded', function () {
     const spinnerElement = document.querySelector('.loading-spinner');
-    spinnerElement.style.borderTopColor = getRandomColor();
-    
-    // Hilfsfunktion für eine zufällige Farbe
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+
+    // Überprüfe, ob das Element gefunden wurde, bevor es verwendet wird
+    if (spinnerElement) {
+        spinnerElement.style.borderTopColor = getRandomColor();
+
+        // Hilfsfunktion für eine zufällige Farbe
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
         }
-        return color;
+    } else {
+        console.error("Das Element mit der Klasse 'loading-spinner' wurde nicht gefunden.");
     }
 });
