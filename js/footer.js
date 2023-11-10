84615c89fb32c0af1d8a0f443e40596fe62ce1c5
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-10 04:27:23 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,5 @@
 // footer.js
 document.addEventListener('DOMContentLoaded', function () {
-    // JavaScript-Code für die Aktivierung und Deaktivierung des Ladebildschirms
-    const loadingScreen = document.getElementById('loading-screen');
-    const content = document.querySelector('body');
-
-    // Warte auf das 'load'-Ereignis (Seite ist vollständig geladen)
-    window.addEventListener('load', function () {
-        // Verstecke den Ladebildschirm und zeige den Content
-        loadingScreen.classList.add('hidden');
-        content.classList.remove('hidden');
-    });
 
     // Das Ziel-Element, in das der Footer geladen werden soll
     var footerContainer = document.getElementById('footer-container');
