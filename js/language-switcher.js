c548aa5d32bd280b83759a945b4d869d54acc253
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-11 02:26:36 +0100

    save 2

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (key && value) {
                     const element = document.getElementById(key);
                     if (element) {
-                        element.textContent = value;
+                        element.innerHTML = value;
                     }
                 }
             });
 
-            // Aktualisiere auch die Texte der Navbar
-            updateNavbarTexts(texts, language);
+            // Aktualisiere auch die Texte
+            updateTexts(texts, language);
         } catch (error) {
             console.error('Fehler beim Laden der Texte:', error);
         }
@@ -31,60 +31,115 @@ document.addEventListener('DOMContentLoaded', function () {
         return response;
     }
 
-    function updateNavbarTexts(texts, language) {
+    function updateTexts(texts, language) {
         // Array mit den IDs der Navbar-Elemente
-        const navbarElementIds = ['home-link', 'about-link', 'contact-link', 'privacy-link', 'imprint-link'];
+        const ElementIds = ['home-link', 'about-link', 'contact-link', 'privacy-link', 'imprint-link', 'tiktok-link', 'instagram-link', 'discord-link', 'github-link'];
 
-        navbarElementIds.forEach(function (elementId) {
+        ElementIds.forEach(function (elementId) {
             const element = document.getElementById(elementId);
             if (element) {
                 // Ersetze '-link' am Ende der ID mit '' (leerem String) für die entsprechende Text-ID
                 const textId = elementId.replace('-link', '');
                 const newText = getTextValue(textId, texts);
-                element.textContent = newText;
+                element.innerHTML = newText;
             }
         });
+
+        // Überprüfe, ob der Slider auf Englisch steht und lade entsprechende Texte
+        async function loadDynamicTexts() {
+            const linkTexts = await getLinkTexts();
+            const tikTokLink = getTextValue('tiktok-link', linkTexts);
+            const instagramLink = getTextValue('instagram-link', linkTexts);
+            const discordLink = getTextValue('discord-link', linkTexts);
+            const githubLink = getTextValue('github-link', linkTexts);
+
+            // Füge Event Listener für die Links hinzu
+            document.getElementById('tiktok-link').addEventListener('click', function () {
+                checkLink('TikTok', tikTokLink);
+            });
+
+            document.getElementById('instagram-link').addEventListener('click', function () {
+                checkLink('Instagram', instagramLink);
+            });
+
+            document.getElementById('discord-link').addEventListener('click', function () {
+                checkLink2('Discord', discordLink);
+            });
+
+            document.getElementById('github-link').addEventListener('click', function () {
+                checkLink2('Github', githubLink);
+            });
+        }
+
+        function getTextValue(textId, texts) {
+            // Finde die Zeile in den Texten, die mit der gewünschten ID beginnt
+            const regex = new RegExp(`^${textId}=`);
+
+            // Filtere die Zeilen basierend auf der ID
+            const matchingLines = texts.split('\n').filter(line => regex.test(line));
+
+            // Extrahiere den Wert aus der Zeile (alles nach dem Gleichheitszeichen)
+            if (matchingLines.length > 0) {
+                return matchingLines[0].split('=')[1];
+            }
+
+            // Wenn keine Übereinstimmung gefunden wurde, gib eine leere Zeichenfolge zurück
+            return '';
+        }
+
+        function updateLanguageCookie(language) {
+            setCookie('language', language, 4); // Speichere die Sprache für 4 Tage
+        }
+
+        function setSliderState(language) {
+            languageSlider.checked = language === 'en';
+        }
+
+        // Initialisiere den Text basierend auf dem gespeicherten Cookie
+        const savedLanguage = getCookie('language') || 'de';
+        setSliderState(savedLanguage);
+
+        // Lade die Texte basierend auf der gespeicherten Sprache
+        loadTexts(savedLanguage);
+
+        // Überwache Änderungen am Schieberegler und speichere die Sprache als Cookie
+        languageSlider.addEventListener('change', function () {
+            const selectedLanguage = languageSlider.checked ? 'en' : 'de';
+
+            // Aktualisiere den Text der Elemente basierend auf der ausgewählten Sprache
+            loadTexts(selectedLanguage);
+
+            // Speichere die ausgewählte Sprache im Cookie
+            updateLanguageCookie(selectedLanguage);
+        });
+
+        loadDynamicTexts();
     }
 
-    function getTextValue(textId, texts) {
-        // Finde die Zeile in den Texten, die mit der gewünschten ID beginnt
-        const regex = new RegExp(`^${textId}=`);
+    // Funktion zum Abrufen der Link-Texte
+    async function getLinkTexts() {
+        const savedLanguage = getCookie('language') || 'de';
+        const response = await fetchTexts(savedLanguage);
+        const texts = await response.text();
+        return texts;
+    }
 
-        // Filtere die Zeilen basierend auf der ID
-        const matchingLines = texts.split('\n').filter(line => regex.test(line));
+    function checkLink(platform, linkText) {
+        alert(linkText || getText('page-not-exist-alert'));
+    }
 
-        // Extrahiere den Wert aus der Zeile (alles nach dem Gleichheitszeichen)
+    function checkLink2(platform, linkText) {
+        alert(linkText || getText('redirect-alert'));
+    }
+
+    // Funktion zum Abrufen des Texts aus der zentralen Datei
+    async function getText(textId) {
+        const savedLanguage = getCookie('language') || 'de';
+        const texts = await fetchTexts(savedLanguage);
+        const matchingLines = texts.split('\n').filter(line => line.startsWith(`${textId}=`));
         if (matchingLines.length > 0) {
             return matchingLines[0].split('=')[1];
         }
-
-        // Wenn keine Übereinstimmung gefunden wurde, gib eine leere Zeichenfolge zurück
         return '';
     }
-
-    function updateLanguageCookie(language) {
-        setCookie('language', language, 4); // Speichere die Sprache für 4 Tage
-    }
-
-    function setSliderState(language) {
-        languageSlider.checked = language === 'en';
-    }
-
-    // Initialisiere den Text basierend auf dem gespeicherten Cookie
-    const savedLanguage = getCookie('language') || 'de';
-    setSliderState(savedLanguage);
-
-    // Lade die Texte basierend auf der gespeicherten Sprache
-    loadTexts(savedLanguage);
-
-    // Überwache Änderungen am Schieberegler und speichere die Sprache als Cookie
-    languageSlider.addEventListener('change', function () {
-        const selectedLanguage = languageSlider.checked ? 'en' : 'de';
-
-        // Aktualisiere den Text der Elemente basierend auf der ausgewählten Sprache
-        loadTexts(selectedLanguage);
-
-        // Speichere die ausgewählte Sprache im Cookie
-        updateLanguageCookie(selectedLanguage);
-    });
 });
