40907bc2e1564e9c11ec334964c8b5b4f1779cdd
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-19 06:21:27 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,34 +1,44 @@
 // footer.js
-document.addEventListener('DOMContentLoaded', async function () {
+document.addEventListener('DOMContentLoaded', function () {
+    // Finde das HTML-Element, in dem der Footer angezeigt werden soll
     const footerContainer = document.getElementById('footer-container');
 
+    // Funktion, um die aktuelle Uhrzeit im Format "Stunde:Minute" zu erhalten
+    function getCurrentTime() {
+        // Erstelle ein neues Date-Objekt, um die aktuelle Uhrzeit zu erhalten
+        const now = new Date();
+
+        // Extrahiere Stunden und füge eine führende Null hinzu, falls nötig
+        const hours = now.getHours().toString().padStart(2, '0');
+
+        // Extrahiere Minuten und füge eine führende Null hinzu, falls nötig
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+
+        // Kombiniere Stunden und Minuten im Format "Stunde:Minute"
+        return `${hours}:${minutes}`;
+    }
+
+    // Aktualisiere die Uhrzeit alle 60 Sekunden (1 Minute)
+    setInterval(() => {
+        const currentTime = getCurrentTime();
+        footerContainer.querySelector('.footer-time').textContent = currentTime;
+    }, 60000);
+
+    // Aktuelles Jahr extrahieren
     const currentYear = new Date().getFullYear();
 
-    try {
-        // Zeitzone des Besuchers abrufen
-        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    // Aktuelle Uhrzeit extrahieren
+    const currentTime = getCurrentTime();
 
-        // Optionen für die Formatierung der Zeit
-        const timeOptions = {
-            hour: 'numeric',
-            minute: '2-digit',
-            timeZone: timeZone
-        };
+    // HTML-Code für den Footer erstellen, einschließlich des aktuellen Jahres und der Uhrzeit
+    const footerHtml = `
+        <footer>
+            <div class="footer-bottom">
+                &copy; 2016 - ${currentYear} | Spiele-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten | Aktuelle Uhrzeit: <span class="footer-time">${currentTime}</span>
+            </div>
+        </footer>
+    `;
 
-        // Aktuelle Zeit in der Zeitzone des Besuchers
-        const currentTime = new Date().toLocaleTimeString('de-DE', timeOptions);
-
-        const footerHtml = 
-        `
-            <footer>
-                <div class="footer-bottom">
-                    &copy; 2016 - ${currentYear} | Spiele-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten | Aktuelle Zeit: ${currentTime}
-                </div>
-            </footer>
-        `;
-
-        footerContainer.innerHTML = footerHtml;
-    } catch (error) {
-        console.error('Fehler beim Abrufen der Zeitzone:', error);
-    }
+    // Setze den erstellten HTML-Code in das Footer-Element ein
+    footerContainer.innerHTML = footerHtml;
 });
