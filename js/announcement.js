58b7061a8fb72e935f435fa6dde6c3bba01f542c
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-19 20:38:11 +0100

    banner

--- js/announcement.js
+++ js/announcement.js
@@ -1,45 +1,50 @@
 // Ankündigungsbanner.js
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Banner-Informationen
+    const bannerName = 'Aktion';
+    const bannerVersion = 'v0.0.0.1';
+
+    // DOM-Elemente
     const announcementBanner = document.getElementById('announcement-banner');
     const closeButton = document.getElementById('close-announcement');
 
-    // Zeitpunkt der letzten Anzeige des Banners
-    const lastDisplayTime = CookieUtil.getCookie('lastBannerDisplayTime');
-
-    // Prüfen, ob das Banner geschlossen wurde
-    const bannerClosed = CookieUtil.getCookie('announcementBannerClosed');
-
-    // Aktueller Zeitpunkt
+    // Cookies und Zeitpunkte
+    const bannerClosed = CookieUtil.getCookie(`${bannerName}Closed`);
+    const lastCloseTime = parseInt(CookieUtil.getCookie(`${bannerName}LastCloseTime`));
     const currentTime = new Date().getTime();
+    const reloadTime = performance.timeOrigin;
 
-    // Zeitraum, nach dem das Banner erneut angezeigt wird (4 Tage)
-    const fourDaysInMilliseconds = 4 * 24 * 60 * 60 * 1000;
-
-    // Dateiversion (kann bei Bedarf mit der neuesten Dateiversion aktualisiert werden)
-    const fileVersion = 'v0.0.0.1';
-
-    // Wenn das Banner noch nie angezeigt wurde oder seit mehr als 4 Tagen nicht mehr angezeigt wurde und nicht geschlossen wurde
-    if (!lastDisplayTime || (currentTime - lastDisplayTime > fourDaysInMilliseconds && !bannerClosed)) {
-        // Hier prüfen, ob die gespeicherte Dateiversion mit der aktuellen übereinstimmt
-        const savedFileVersion = CookieUtil.getCookie('fileVersion');
-        if (savedFileVersion !== fileVersion) {
-            announcementBanner.style.display = 'block'; // Banner anzeigen
-        }
+    // Funktion zur Überprüfung, ob die Seite neu geladen wurde
+    function isPageReloaded() {
+        return currentTime - reloadTime < 1000; // Annahme: Wenn die Seite innerhalb von 1 Sekunde neu geladen wurde
     }
 
-    // Prüfen, ob wir uns auf der Hauptseite befinden und das Banner noch nie angezeigt wurde
-    const isHomePage = window.location.pathname === '/';
-    if (isHomePage && !lastDisplayTime) {
-        announcementBanner.style.display = 'block'; // Banner anzeigen
+    // Funktion zum Anzeigen des Banners
+    function showBanner() {
+        announcementBanner.style.display = 'flex';
+    }
+
+    // Überprüfen, ob das Banner geschlossen wurde und nicht für die aktuelle Sitzung angezeigt werden soll
+    if (!bannerClosed || (lastCloseTime && currentTime - lastCloseTime > 1000 * 60 * 60 * 24 && !isPageReloaded())) {
+        showBanner();
     }
 
     // Event-Listener für den Schließen-Button des Banners
     closeButton.addEventListener('click', function () {
-        announcementBanner.parentNode.removeChild(announcementBanner); // Banner ausblenden
-        // Setze Cookies für geschlossenes Banner, Zeitpunkt der Schließung und Dateiversion mit Ablaufdatum
-        CookieUtil.setCookie('announcementBannerClosed', 'true', 4);  // Banner wird für 4 Tage geschlossen
-        CookieUtil.setCookie('lastBannerDisplayTime', currentTime, 1);  // Banner wird für 1 Tag nicht erneut angezeigt
-        CookieUtil.setCookie('fileVersion', fileVersion, 365);  // Dateiversion wird für 365 Tage gespeichert (kann angepasst werden)
+        // Setzen der Cookies und Zeitpunkte
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion} Closed`, 'true', 1); // Banner wird für 1 Tag geschlossen
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion} LastCloseTime`, currentTime.toString(), 365); // Zeitpunkt der Schließung für 1 Jahr speichern
+
+        // Ausblenden des Banners
+        announcementBanner.style.display = 'none';
     });
+
+    // Überprüfen, ob sich die Banner-Version geändert hat und das Banner anzeigen
+    const savedBannerVersion = CookieUtil.getCookie(`${bannerName}-${bannerVersion}`);
+    if (savedBannerVersion !== bannerVersion) {
+        showBanner();
+        // Aktualisierte Banner-Version speichern
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion}`, 365); // Banner-Version wird für 1 Jahr gespeichert
+    }
 });
