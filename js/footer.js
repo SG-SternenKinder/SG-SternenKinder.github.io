d050af422be0243a4f046359c89fa74fca49f42b
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-19 06:08:49 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,17 +1,34 @@
 // footer.js
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     const footerContainer = document.getElementById('footer-container');
 
     const currentYear = new Date().getFullYear();
-    const currentTime = new Date().toLocaleTimeString();
 
-    const footerHtml = `
-        <footer>
-            <div class="footer-bottom">
-                &copy; 2016 - ${currentYear} | Spiele-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten | ${currentTime}
-            </div>
-        </footer>
-    `;
+    try {
+        // Zeitzone des Besuchers abrufen
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    footerContainer.innerHTML = footerHtml;
+        // Optionen für die Formatierung der Zeit
+        const timeOptions = {
+            hour: 'numeric',
+            minute: '2-digit',
+            timeZone: timeZone
+        };
+
+        // Aktuelle Zeit in der Zeitzone des Besuchers
+        const currentTime = new Date().toLocaleTimeString('de-DE', timeOptions);
+
+        const footerHtml = 
+        `
+            <footer>
+                <div class="footer-bottom">
+                    &copy; 2016 - ${currentYear} | Spiel-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten | Aktuelle Zeit: ${currentTime}
+                </div>
+            </footer>
+        `;
+
+        footerContainer.innerHTML = footerHtml;
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Zeitzone:', error);
+    }
 });
