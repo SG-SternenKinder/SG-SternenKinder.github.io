7a48a51b54149124eabe226aefbfb3fbf7466d3e
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-19 06:06:04 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,11 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const footerContainer = document.getElementById('footer-container');
 
     const currentYear = new Date().getFullYear();
+    const currentTime = new Date().toLocaleTimeString();
 
     const footerHtml = `
         <footer>
             <div class="footer-bottom">
-                &copy; 2016 - ${currentYear} | Spiel-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten
+                &copy; 2016 - ${currentYear} | Spiele-Gemeinschaft (SG) SternenKinder | Alle Rechte vorbehalten | ${currentTime}
             </div>
         </footer>
     `;
