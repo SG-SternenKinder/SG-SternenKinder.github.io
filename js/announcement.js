4ea78afc36b061430da4cf09f0efa90258d39b01
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-21 11:00:31 +0100

    Update announcement.js

--- js/announcement.js
+++ js/announcement.js
@@ -2,22 +2,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Banner-Informationen
     const bannerName = 'Aktion';
-    const bannerVersion = 'v0.0.0.2.3';
+    const bannerVersion = 'v0.0.0.2.4';
 
     // DOM-Elemente
     const announcementBanner = document.getElementById('announcement-banner');
     const closeButton = document.getElementById('close-announcement');
 
     // Cookies und Zeitpunkte
-    const bannerClosed = CookieUtil.getCookie(`${bannerName}-${bannerVersion}Closed`);
-    const lastCloseTime = parseInt(CookieUtil.getCookie(`${bannerName}-${bannerVersion}LastCloseTime`));
+    const bannerClosed = CookieUtil.getCookie(`${bannerName}-${bannerVersion}-Closed`);
+    const lastCloseTime = parseInt(CookieUtil.getCookie(`${bannerName}-${bannerVersion}-LastCloseTime`));
     const currentTime = new Date().getTime();
-    const reloadTime = performance.timeOrigin;
-
-    // Funktion zur Überprüfung, ob die Seite neu geladen wurde
-    function isPageReloaded() {
-        return currentTime - reloadTime < 1000; // Annahme: Wenn die Seite innerhalb von 1 Sekunde neu geladen wurde
-    }
 
     // Funktion zum Anzeigen des Banners
     function showBanner() {
@@ -27,8 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Überprüfen, ob das Banner geschlossen wurde und nicht für die aktuelle Sitzung angezeigt werden soll
-    if (!bannerClosed || (lastCloseTime && currentTime - lastCloseTime > 1000 * 60 * 60 * 24 && !isPageReloaded())) {
+    // Überprüfen, ob das Banner geschlossen wurde
+    if (!bannerClosed || (lastCloseTime && currentTime - lastCloseTime > 1000 * 60 * 60 * 24 )) {
         showBanner();
     } else {
         if (consoleManager.getConsoleOutput()) {
@@ -39,8 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Event-Listener für den Schließen-Button des Banners
     closeButton.addEventListener('click', function () {
         // Setzen der Cookies und Zeitpunkte
-        CookieUtil.setCookie(`${bannerName}-${bannerVersion}Closed`, 'true', 2, { secure: true }); // Banner wird für 1 Tag geschlossen
-        CookieUtil.setCookie(`${bannerName}-${bannerVersion}LastCloseTime`, currentTime.toString(), 365, { secure: true }); // Zeitpunkt der Schließung für 1 Jahr speichern
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion}-Closed`, 'true', 2, { secure: true }); // Banner wird für 1 Tag geschlossen
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion}-LastCloseTime`, currentTime.toString(), 365, { secure: true }); // Zeitpunkt der Schließung für 1 Jahr speichern
 
         // Ausblenden des Banners
         announcementBanner.style.display = 'none';
@@ -50,11 +44,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Überprüfen, ob sich die Banner-Version geändert hat und das Banner anzeigen
-    const savedBannerClosed = CookieUtil.getCookie(`${bannerName}-${bannerVersion}Closed`);
+    const savedBannerClosed = CookieUtil.getCookie(`${bannerName}-${bannerVersion}-Closed`);
     if (savedBannerClosed !== 'true') {
         showBanner();
         // Aktualisierte Banner-Version speichern
-        CookieUtil.setCookie(`${bannerName}-${bannerVersion}Closed`, 'true', 1, { secure: true }); // Banner wird für 1 Tag geschlossen
+        CookieUtil.setCookie(`${bannerName}-${bannerVersion}-Closed`, 'true', 2, { secure: true }); // Banner wird für 1 Tag geschlossen
         if (consoleManager.getConsoleOutput()) {
             console.log('Banner wird angezeigt, da sich die Version geändert hat.');
         }
