a6ee7b2ea13a120138f0c6a571061c39340a8811
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-21 13:55:16 +0100

    Update announcement.js

--- js/announcement.js
+++ js/announcement.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Banner-Informationen
     const bannerName = 'Aktion';
-    const bannerVersion = 'v0.0.0.2.7';
+    const bannerVersion = 'v0.0.0.2.8';
 
     // DOM-Elemente
     const announcementBanner = document.getElementById('announcement-banner');
@@ -19,12 +19,38 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Überprüfen, ob das Banner geschlossen wurde
-    if (!bannerClosed) {
+    // Überprüfen, ob die Seite innerhalb von 1 Sekunde neu geladen wurde und der Benutzer von einer anderen Seite zurückkehrt
+    const reloadTime = performance.timeOrigin;
+    const currentTime = new Date().getTime();
+    const isReload = currentTime - reloadTime < 1000;
+
+    // Überprüfen, ob die Seite innerhalb von 1 Sekunde neu geladen wurde und es einen Referrer gibt (z.B. der Benutzer kehrt von einer anderen Seite zurück)
+    if (isReload && document.referrer) {
+        // Seite wurde innerhalb von 1 Sekunde neu geladen und es gibt einen Referrer (z.B. der Benutzer kehrt von einer anderen Seite zurück)
+        // Banner wieder anzeigen
         showBanner();
-    } else {
         if (consoleManager.getConsoleOutput()) {
-            console.log('Banner wird nicht angezeigt.');
+            console.log('Banner wird angezeigt, da die Seite innerhalb von 1 Sekunde neu geladen wurde.');
+        }
+    } else {
+        // Überprüfen, ob das Banner geschlossen wurde
+        if (!bannerClosed) {
+            showBanner();
+        } else {
+            // Überprüfen, ob sich die Banner-Version geändert hat und das Banner anzeigen
+            const savedBannerVersion = localStorage.getItem(`${bannerName}-LastVersion`);
+            if (savedBannerVersion !== bannerVersion) {
+                showBanner();
+                // Aktualisierte Banner-Version speichern
+                localStorage.setItem(`${bannerName}-LastVersion`, bannerVersion);
+                if (consoleManager.getConsoleOutput()) {
+                    console.log('Banner wird angezeigt, da sich die Version geändert hat.');
+                }
+            } else {
+                if (consoleManager.getConsoleOutput()) {
+                    console.log('Banner wird nicht angezeigt, da die Version gleich ist und das Banner geschlossen wurde.');
+                }
+            }
         }
     }
 
@@ -39,34 +65,4 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log('Banner wurde geschlossen.');
         }
     });
-
-    // Überprüfen, ob sich die Banner-Version geändert hat und das Banner anzeigen
-    const savedBannerVersion = localStorage.getItem(`${bannerName}-LastVersion`);
-    if (savedBannerVersion !== bannerVersion) {
-        showBanner();
-        // Aktualisierte Banner-Version speichern
-        localStorage.setItem(`${bannerName}-LastVersion`, bannerVersion);
-        if (consoleManager.getConsoleOutput()) {
-            console.log('Banner wird angezeigt, da sich die Version geändert hat.');
-        }
-    } else {
-        if (consoleManager.getConsoleOutput()) {
-            console.log('Banner wird nicht angezeigt, da die Version gleich ist.');
-        }
-    }
-
-    // Überprüfen, ob die Seite neu geladen wurde und der Benutzer von einer anderen Seite zurückkehrt
-    const reloadTime = performance.timeOrigin;
-    const currentTime = new Date().getTime();
-    const isReload = currentTime - reloadTime < 1000;
-
-    // Überprüfen, ob die Seite innerhalb von 1 Sekunde neu geladen wurde und es einen Referrer gibt (z.B. der Benutzer kehrt von einer anderen Seite zurück)
-    if (isReload && document.referrer) {
-        // Seite wurde innerhalb von 1 Sekunde neu geladen und es gibt einen Referrer (z.B. der Benutzer kehrt von einer anderen Seite zurück)
-        // Banner wieder anzeigen
-        showBanner();
-        if (consoleManager.getConsoleOutput()) {
-            console.log('Banner wird angezeigt, da die Seite innerhalb von 1 Sekunde neu geladen wurde.');
-        }
-    }
 });
