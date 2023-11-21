ced8363c7b8315e7f4c925d14f165da8f92c3531
OsbornTV <22799027+OsbornTV@users.noreply.github.com>
2023-11-21 14:19:34 +0100

    Update announcement.js

--- js/announcement.js
+++ js/announcement.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Banner-Informationen
     const bannerName = 'Aktion';
-    const bannerVersion = 'v0.0.0.3.1';
+    const bannerVersion = 'v0.0.0.3.2';
 
     // DOM-Elemente
     const announcementBanner = document.getElementById('announcement-banner');
@@ -11,31 +11,32 @@ document.addEventListener('DOMContentLoaded', function () {
     // Überprüfen, ob das Banner geschlossen wurde
     const bannerClosed = localStorage.getItem(`${bannerName}-${bannerVersion}-Closed`);
 
-    // Überprüfen, ob das Banner aufgrund einer neuen Version oder Schließung angezeigt werden soll
-    if (!bannerClosed) {
-        // Hier könntest du weitere Bedingungen hinzufügen, z.B. Zeitintervalle
-        // bevor das Banner erneut angezeigt wird, um zu verhindern, dass es bei jedem Laden erscheint.
-
-        // Banner anzeigen
+    // Funktion zum Anzeigen des Banners
+    function showBanner() {
         announcementBanner.style.display = 'block';
         if (consoleManager.getConsoleOutput()) {
             console.log('Banner wird angezeigt.');
         }
+    }
+
+    // Überprüfen, ob das Banner aufgrund einer neuen Version oder Schließung angezeigt werden soll
+    if (!bannerClosed) {
+        showBanner();
+
+        // Event-Listener für den Schließen-Button des Banners
+        closeButton.addEventListener('click', function () {
+            // Setzen des Flags im localStorage, dass das Banner geschlossen wurde
+            localStorage.setItem(`${bannerName}-${bannerVersion}-Closed`, 'true');
+
+            // Ausblenden des Banners
+            announcementBanner.style.display = 'none';
+            if (consoleManager.getConsoleOutput()) {
+                console.log('Banner wurde geschlossen.');
+            }
+        });
     } else {
         if (consoleManager.getConsoleOutput()) {
             console.log('Banner wird nicht angezeigt, da es geschlossen wurde.');
         }
     }
-
-    // Event-Listener für den Schließen-Button des Banners
-    closeButton.addEventListener('click', function () {
-        // Setzen des Flags im localStorage, dass das Banner geschlossen wurde
-        localStorage.setItem(`${bannerName}-${bannerVersion}-Closed`, 'true');
-
-        // Ausblenden des Banners
-        announcementBanner.style.display = 'none';
-        if (consoleManager.getConsoleOutput()) {
-            console.log('Banner wurde geschlossen.');
-        }
-    });
 });
