563449ad995f55ba39cd6f07cd53abbf0f9708e8
OstbornTV <22799027+OstbornTV@users.noreply.github.com>
2024-09-12 18:57:07 +0200

    Update background.js

--- js/background.js
+++ js/background.js
@@ -6,10 +6,8 @@ $(document).ready(function() {
         const depth = Math.max(pathParts.length - 1, 0); // Sicherstellen, dass depth nicht negativ ist
         return '../'.repeat(depth);
     }
-
     // Basis-Pfad ermitteln
     const basePath = getBasePath();
-
     // Array mit Bild-URLs
     const images = [
         `${basePath}img/background.png`,
@@ -17,41 +15,28 @@ $(document).ready(function() {
         `${basePath}img/background3.png`
         // Füge hier weitere Bild-URLs hinzu
     ];
-
     // Der Fallback-Image-URL (immer vorhandenes Bild)
     const fallbackImage = `${basePath}img/background.png`; // Ändere dies, falls nötig
-
     // Funktion zum Überprüfen, ob das Bild existiert
     function checkImage(url) {
-        return $.Deferred(function(defer) {
-            const img = new Image();
-            img.onload = () => defer.resolve(true);
-            img.onerror = () => defer.resolve(false);
-            img.src = url;
-        }).promise();
+        return new Promise((resolve) => {
+            $('<img>').attr('src', url).on('load', () => resolve(true)).on('error', () => resolve(false));
+        });
     }
-
     // Funktion zum Zufallsbild auswählen
-    function setRandomBackgroundImage() {
+    async function setRandomBackgroundImage() {
         const randomIndex = Math.floor(Math.random() * images.length);
         const imageUrl = images[randomIndex];
-
         // Überprüfen, ob das Bild existiert
-        checkImage(imageUrl).done(function(exists) {
-            const imageToUse = exists ? imageUrl : fallbackImage;
-            if (!exists) {
-                // Zeige Fehlermeldung oder mache etwas anderes
-                showErrorPopup('Das Hintergrundbild konnte nicht geladen werden. Fallback verwendet.');
-            }
+        const exists = await checkImage(imageUrl);
+        
+        const imageToUse = exists ? imageUrl : fallbackImage;
+        if (!exists) {
+            showErrorPopup('Das Hintergrundbild konnte nicht geladen werden. Fallback verwendet.');
+        }
 
-            // Erstelle ein neues Bild-Element und setze es als Hintergrundbild
-            const $backgroundImage = $('<img>', { src: imageToUse, id: 'fsb_image', style: 'display: none;' }).on('load', function() {
-                $('body').css('background-image', `url(${imageToUse})`);
-                $(this).remove(); // Entferne das Bild nach dem Laden
-            });
-
-            $('body').append($backgroundImage);
-        });
+        // Bild hinzufügen
+        $('body').append(`<img src="${imageToUse}" id="fsb_image" alt=""/>`);
     }
 
     // Beim Laden der Seite zufälliges Hintergrundbild setzen
