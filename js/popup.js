1d0a3a33622756fb5dc0a999c5e72f78fa40c61f
OstbornTV <22799027+OstbornTV@users.noreply.github.com>
2024-09-12 17:48:29 +0200

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     }
 
     function loadPopupTexts(callback) {
-        $.getJSON('json/popup.json', function(data) { // Pfad anpassen
+        $.getJSON('../json/popup.json', function(data) {
             callback(data);
         });
     }
